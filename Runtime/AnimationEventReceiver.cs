b2ca595e5b7670ddf989301f9be20aee3a55aba6
Brian Knep <bk@blep.com>
2026-02-08 16:12:40 -0500

    Listeners can not be added and removed from code

--- Runtime/AnimationEventReceiver.cs
+++ Runtime/AnimationEventReceiver.cs
@@ -1,12 +1,35 @@
 using UnityEngine;
+using UnityEngine.Events;
 using System.Collections.Generic;
 
 namespace ImprovedUnityAnimationEvents {
     public class AnimationEventReceiver : MonoBehaviour {
         [SerializeField] List<AnimationEvent> animationEvents = new();
 
+        AnimationEvent GetEvent(string eventName) =>
+            animationEvents.Find(se => se.eventName == eventName);
+
+        public void AddListener(string eventName, UnityAction call) {
+            var animationEvent = GetEvent(eventName);
+            if (animationEvent == null) {
+                animationEvent = new AnimationEvent() {
+                    eventName = eventName,
+                    OnAnimationEvent = new ()
+                };
+                animationEvents.Add(animationEvent);
+            }
+            animationEvent.OnAnimationEvent.AddListener(call);
+        }
+
+        public void RemoveListener(string eventName, UnityAction call) {
+            var animationEvent = GetEvent(eventName);
+            Debug.Assert(animationEvent != null,
+                         $"Trying to remove listener from non-existent animation event '{eventName}'");
+            animationEvent.OnAnimationEvent.RemoveListener(call);
+        }
+
         public void OnAnimationEventTriggered(string eventName) {
-            AnimationEvent matchingEvent = animationEvents.Find(se => se.eventName == eventName);
+            AnimationEvent matchingEvent = GetEvent(eventName);
             matchingEvent?.OnAnimationEvent?.Invoke();
         }
     }
