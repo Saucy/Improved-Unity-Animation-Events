08828571434ba2cca08b47964680fc3a0a7dae9b
Brian Knep <bk@blep.com>
2026-02-08 16:06:28 -0500

    Added repeating events; fixed triggerTime = 0

--- Runtime/AnimationEventStateBehaviour.cs
+++ Runtime/AnimationEventStateBehaviour.cs
@@ -5,21 +5,21 @@ namespace ImprovedUnityAnimationEvents {
     public class AnimationEventStateBehaviour : StateMachineBehaviour {
         public string eventName;
         [Range(0f, 1f)] public float triggerTime;
-        
-        bool hasTriggered;
+        public bool repeat;
+
+        float nextTriggerTime;
         AnimationEventReceiver receiver;
 
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-            hasTriggered = false;
+            nextTriggerTime = triggerTime;
             receiver = animator.GetComponent<AnimationEventReceiver>();
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-            float currentTime = stateInfo.normalizedTime % 1f;
-
-            if (!hasTriggered && currentTime >= triggerTime) {
+            float currentTime = stateInfo.normalizedTime;
+            if (currentTime > nextTriggerTime) {
                 NotifyReceiver(animator);
-                hasTriggered = true;
+                nextTriggerTime = repeat ? nextTriggerTime + 1 : float.MaxValue;
             }
         }
 
